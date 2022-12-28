b28cd482d1c3773d21db455eaf66a670a776e9fb
your-name <your-email-address>
2022-12-29 00:57:50 +0300

    added login and register

--- errands/views.py
+++ errands/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'errands/index.html')
+
+def create_errands(request):
+    return render(request, 'errands/create-errands.html')
--- errandsite/urls.py
+++ errandsite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('errands.urls')),
+    path('auth', include('authentication.urls')),
 ]
