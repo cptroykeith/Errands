290cb13c7300b512394d6faee0f724a46c60ed48
your-name <your-email-address>
2022-12-30 01:50:08 +0300

    user login

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from validate_email import validate_email
 from .models import User
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 
 
@@ -50,5 +52,23 @@ def  register(request):
         
     return render(request,'authentication/register.html')
 
-def  login(request):
+def  login_user(request):
+
+    if request.method  == 'POST':
+        context={'data':request.POST}
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user=authenticate(request,username=username, password=password)
+
+        if not user:
+            messages.add_message(request, messages.ERROR, 'Invalid credentials')
+            return render(request,'authentication/login.html',context)
+
+        login(request, user)
+
+        messages.add_message(request, messages.SUCCESS, f'Welcome {user.username}')
+
+        return redirect(reverse('home'))
+
     return render(request,'authentication/login.html')
