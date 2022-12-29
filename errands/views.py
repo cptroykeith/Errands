982ca6e9cfaaf38b05c2f8ab0e3e531c638af2c0
your-name <your-email-address>
2022-12-29 17:21:17 +0300

    filtering querysets

--- errands/views.py
+++ errands/views.py
@@ -4,14 +4,24 @@ from .models import errands
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-# Create your views here.
+def get_showing_Errands(request, Errands):
+
+    if request.GET and request.GET.get('filter'):
+        if request.GET.get('filter') == 'complete':
+            return Errands.filter(is_completed=True)
+        if request.GET.get ('filter') == 'incomplete':
+            return Errands.filter(is_completed=False)
+    return Errands
+
 def index(request):
     Errands = errands.objects.all()
 
-    completed_count=Errands,filter(is_completed=True).count()
-    incompleted_count=Errands,filter(is_completed=False).count()
+    completed_count = Errands.filter(is_completed=True).count()
+    incomplete_count = Errands.filter(is_completed=False).count()
+    all_count = Errands.count()
 
-    context = {'Errands' : Errands}
+    incomplete_count = Errands.filter(is_completed=False).count()
+    context = {'Errands' : get_showing_Errands(request, Errands), 'all_count' :all_count, 'completed_count': completed_count, 'incomplete_count': incomplete_count}
     return render(request, 'errands/index.html',context)
 
 def create_errands(request):
