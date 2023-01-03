e363ecd0fddbe1daba15bdeb2f00cbf6bc0b9007
your-name <your-email-address>
2023-01-03 21:47:16 +0300

    email verification in Registration

--- authentication/views.py
+++ authentication/views.py
@@ -5,7 +5,39 @@ from .models import User
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from helpers.decorator import auth_user_should_not_access
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes,force_str, DjangoUnicodeDecodeError
+from .utils import generate_token
+from django.core.mail import EmailMessage
+from django.conf import settings
+import threading
 
+
+class EmailThread(threading.Thread):
+
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send()
+
+
+def send_activation_email(user,request):
+    current_site= get_current_site(request)
+    email_subject = 'Activate your account'
+    email_body= render_to_string('authentication/activate.html',{
+        'user':user,
+        'domain':current_site,
+        'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+        'token':generate_token.make_token(user)
+    })
+
+    email=EmailMessage(subject=email_subject, body=email_body, from_email=settings.EMAIL_FROM_USER, to=[user.email])
+
+    EmailThread(email).start()
 
 @auth_user_should_not_access
 def  register(request):
@@ -48,7 +80,10 @@ def  register(request):
         user.set_password(password)
         user.save()
 
-        messages.add_message(request,messages.SUCCESS, 'Account created, you can now login')
+
+        send_activation_email(user,request)
+
+        messages.add_message(request,messages.SUCCESS, 'We sent you an email to verify your account')
         return redirect('login')
         
     return render(request,'authentication/register.html')
@@ -61,6 +96,12 @@ def  login_user(request):
         password = request.POST.get('password')
 
         user=authenticate(request,username=username, password=password)
+
+        if not user.is_email_verified:
+            messages.add_message(request, messages.ERROR, 'Email is not verified, please check email inbox')
+            return render(request,'authentication/login.html',context)
+
+
 
         if not user:
             messages.add_message(request, messages.ERROR, 'Invalid credentials')
@@ -82,3 +123,23 @@ def logout_user(request):
     messages.add_message(request, messages.SUCCESS, 'Successfully logged out')
 
     return redirect(reverse('login'))
+
+
+def activate_user(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        
+        user = User.objects.get(pk=uid)
+
+    except Exception as e:
+            user=None
+
+    if user and generate_token.check_token(user,token):
+        user.is_email_verified = True
+        user.save()
+
+        messages.add_message(request, messages.SUCCESS, 'Email verified, you can now login ')
+        return redirect(reverse('login'))
+
+
+    return render(request, 'authentication/activate-failed.html',{"user":user})
